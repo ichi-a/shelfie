98ab437904a4d892dffc72d3d5c45575b61d914d
ichi-a <noichi.sy@gmail.com>
2026-02-07 18:02:55 +0900

    Form追加など

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@ import { GeminiInput } from "@/components/GeminiInput";
 import { AuthButton } from "@/components/ui/AuthButton";
 import { Welcome } from "@/components/ui/WelcomeMessage";
 import { Loading } from "@/components/ui/Load";
+import { DemoApp } from "@/components/ui/DemoApp";
 
 export default async function Home() {
 
 
   return (
-    <div className="p-2 pb-20 bg-[#F5F3EF]">
+    <div className="p-2 bg-[#F5F3EF]">
       <Loading />
       <AuthButton />
       <Welcome />
       <GeminiInput />
-
+      <DemoApp />
 
     </div>
   );
