513de9569e81c61de086b81bd4d071ff00129e53
ichi-a <noichi.sy@gmail.com>
2026-02-09 16:49:14 +0900

    デモ調整

--- src/components/ui/WelcomeMessage.tsx
+++ src/components/ui/WelcomeMessage.tsx
@@ -2,6 +2,11 @@
 import { auth } from "@/lib/firebase"
 import { useEffect, useState } from "react"
 import { User } from "firebase/auth"
+import Image from "next/image"
+import ai_recommend from "@/assets/image/ai_recommend.png"
+import book_detail from "@/assets/image/book_detail.png"
+import my_shelf from "@/assets/image/my_shelf.png"
+import shelf_search from "@/assets/image/shelf_search.png"
 
 export const Welcome = () => {
 
@@ -26,7 +31,7 @@ if (user) return null
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-[50vh] px-4 text-center mx-auto">
+    <div className="max-w-180 flex flex-col items-center justify-center min-h-[50vh] px-4 text-center mx-auto">
       {user ? ("") : (<>
       <h2 className="text-2xl font-bold text-[#1F4D4F] tracking-tight mb-3">
         Welcome to <span className="text-[#C89B3C]">Shelfie</span>
@@ -35,7 +40,20 @@ if (user) return null
         ログインしてShelfieを楽しんでください！
       </p>
       </>)}
-
+        <div className="mx-auto w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 justify-items-center">
+          <div className="w-full max-w-100 aspect-16/12 relative border border-black overflow-hidden">
+            <Image src={ai_recommend} alt="アプリデモ画像" className="object-cover"/>
+          </div>
+          <div className="w-full max-w-100 aspect-16/12 relative border border-black overflow-hidden">
+            <Image src={shelf_search} alt="アプリデモ画像" className="object-cover"/>
+          </div>
+          <div className="w-full max-w-100 aspect-16/12 relative border border-black overflow-hidden">
+            <Image src={my_shelf} alt="アプリデモ画像" fill className="object-cover"/>
+          </div>
+          <div className="w-full max-w-100 aspect-16/12 relative border border-black overflow-hidden">
+            <Image src={book_detail} alt="アプリデモ画像" fill className="object-cover"/>
+          </div>
+        </div>
     </div>
   )
 }
