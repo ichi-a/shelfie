367f435e082f4a1ceb565a166cf8c691e4326d20
ichi-a <noichi.sy@gmail.com>
2026-02-10 10:51:46 +0900

    welcome編集

--- src/components/ui/WelcomeMessage.tsx
+++ src/components/ui/WelcomeMessage.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { User } from "firebase/auth"
 import Image from "next/image"
 import ai_recommend from "@/assets/image/ai_recommend.png"
-import book_detail from "@/assets/image/book_detail.png"
+import book_detail1 from "@/assets/image/book_detail1.png"
 import my_shelf from "@/assets/image/my_shelf.png"
 import shelf_search from "@/assets/image/shelf_search.png"
 
@@ -31,27 +31,33 @@ if (user) return null
 
 
   return (
-    <div className="max-w-180 flex flex-col items-center justify-center min-h-[50vh] px-4 text-center mx-auto">
+    <div className="max-w-420 mt-30 flex flex-col items-center justify-center px-4 text-center mx-auto">
       {user ? ("") : (<>
       <h2 className="text-2xl font-bold text-[#1F4D4F] tracking-tight mb-3">
         Welcome to <span className="text-[#C89B3C]">Shelfie</span>
       </h2>
-      <p className="max-w-xs text-sm text-[#1F4D4F]/60 mb-10">
+      <p className="max-w-xs text-sm text-[#1F4D4F]/60 mb-40">
         ログインしてShelfieを楽しんでください！
       </p>
       </>)}
-        <div className="mx-auto w-10/12 max-w-6xl grid grid-cols-1 md:grid-cols-2 justify-items-center">
-          <div className="w-full max-w-125 aspect-16/12 relative border border-black overflow-hidden">
-            <Image src={ai_recommend} alt="アプリデモ画像" className="object-cover"/>
+        <div className="mx-auto w-10/12 max-w-400 grid grid-cols-1 p-5 gap-5 justify-items-center">
+          <p className="text-xl font-serif font-bold p-1 text-[#1F4D4F]">アプリ紹介</p>
+          <div className="h-1 w-62 bg-[#C89B3C] mx-auto mb-15" />
+          <p className="text-[#1F4D4F]/80 font-medium mt-8">AI司書があなたの本棚を分析しベストな１冊を提案します！</p>
+          <div className="mb-10 w-full max-w-300 aspect-16/12 relative rounded border border-[#1F4D4F]/20 overflow-hidden">
+            <Image src={ai_recommend} alt="アプリデモ画像" fill className="object-cover"/>
           </div>
-          <div className="w-full max-w-125 aspect-16/12 relative border border-black overflow-hidden">
-            <Image src={shelf_search} alt="アプリデモ画像" className="object-cover"/>
+          <p className="text-[#1F4D4F]/80 font-medium mt-8">キーワード検索で本を本棚に追加しましょう！</p>
+          <div className="mb-10 w-full max-w-300 aspect-16/12 relative rounded border border-[#1F4D4F]/20 overflow-hidden">
+            <Image src={shelf_search} alt="アプリデモ画像" fill className="object-cover"/>
           </div>
-          <div className="w-full max-w-1125 aspect-16/12 relative border border-black overflow-hidden">
+          <p className="text-[#1F4D4F]/80 font-medium mt-8">本棚にお気に入りの本を並べましょう！</p>
+          <div className="mb-10 w-full max-w-300 aspect-16/12 relative rounded border border-[#1F4D4F]/20 overflow-hidden">
             <Image src={my_shelf} alt="アプリデモ画像" fill className="object-cover"/>
           </div>
-          <div className="w-full max-w-125 aspect-16/12 relative border border-black overflow-hidden">
-            <Image src={book_detail} alt="アプリデモ画像" fill className="object-cover"/>
+          <p className="text-[#1F4D4F]/80 font-medium mt-8">スコアやコメントを残してAI司書の提案の精度を上げましょう！</p>
+          <div className="mb-10 w-full max-w-300 aspect-16/12 relative rounded border border-[#1F4D4F]/20 overflow-hidden">
+            <Image src={book_detail1} alt="アプリデモ画像" fill className="object-cover"/>
           </div>
         </div>
     </div>
