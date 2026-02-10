06fbb23a7c71b23ba267c27b406a27f9264d33d6
ichi-a <noichi.sy@gmail.com>
2026-02-10 12:10:40 +0900

    インストールボタン調整

--- src/components/ui/InstallBtn.tsx
+++ src/components/ui/InstallBtn.tsx
@@ -9,9 +9,9 @@ export default function PWAbtn() {
   if (!isInstallable) return null;
 
   return (
-    <div className="bg-blue-600 w-full h-300">
-      <button onClick={install} className="text-6xl">
-        インストール
+    <div className="mx-auto">
+      <button onClick={install} className="font-semibold bg-[#1F4D4F] hover:bg-[#1F4D4F]/90 py-2 px-4 text-center tracking-tight text-white hover:text-[#C89B3C] transition-all rounded-sm shadow-sm active:scale-96">
+        HOMEにインストール
       </button>
     </div>
   )
