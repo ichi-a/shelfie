a9bff7d6fcf8b18b1201f05a36e3c0e18281d95d
ichi-a <noichi.sy@gmail.com>
2026-02-22 02:44:00 +0900

    Tally URL変更

--- src/components/ui/TallyForm.tsx
+++ src/components/ui/TallyForm.tsx
@@ -13,6 +13,7 @@ export default function ContactPage() {
     }
   }, []);
 
+  const app = "shelfie"
   return (
     <div className="min-h-screen p-8 max-w-141 mx-auto">
       <h2 className="text-[#1F4D4F] text-lg font-bold mb-4 tracking-tight text-center">
@@ -21,7 +22,7 @@ export default function ContactPage() {
 
       <div className="max-w-3xl mx-auto border border-[#1F4D4F]/10 rounded-xl overflow-hidden shadow-sm">
         <iframe
-          data-tally-src="https://tally.so/embed/EkQzWq?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+          data-tally-src={`https://tally.so/embed/EkQzWq?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1&app_name=${app}`}
           loading="lazy"
           width="100%"
           height="500" // 高さをしっかり指定しておくとガタつきません
